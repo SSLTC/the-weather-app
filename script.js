67c7e7dfd6fe446ddffb930ebdbb414c6493651e
Snoeck Sven <snoeck.sven@live.be>
2023-01-25 16:52:27 +0100

    Add background photo from unsplash

--- script.js
+++ script.js
@@ -31,16 +31,32 @@ console.log(currentDate + ' ' + currentTime);
 let weatherData;
 
 elCity.addEventListener("change", () => {
+    
     getData();
     showWeatherInfo();
+    
 })
+
+const getWeatherRelatedImage = async (city, weather) => {
+    const queryUrl = `https://api.unsplash.com/search/photos?client_id=kffooHDtQGZv7a36Az_EaHeWT4CW4D8W3ZINMVOn0hM&query='${city}+${weather}'`;
+
+    try {
+        const result = await fetch(queryUrl);
+        const data = await result.json();
+
+        document.body.style.background = `url(${data.results[0].urls.regular})`;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 const getData = async () => {
     const selectedCity = elCity.options[elCity.selectedIndex].text;
     const unit = "metric";
     // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     //const queryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=${apiKey}`;
-    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&unit=${unit}&appid=${apiKey}`;
+    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=${unit}&appid=${apiKey}`;
 
     try {
         const result = await fetch(queryUrl);
@@ -48,7 +64,10 @@ const getData = async () => {
         console.log(data);
 
         const elH1WeatherInfo = document.createElement("h1");
-        elH1WeatherInfo.innerHTML = data.name + " (" + data.sys.country + ")" + "<br/>" + getDateTime(data.dt);
+        elH1WeatherInfo.innerHTML = data.name 
+            + " (" + data.sys.country + ")" + "<br/>"
+            + data.weather[0].main + " " + data.main.temp_min + " deg. - " + data.main.temp_max + " deg."  
+            + "<br/>" + getDateTime(data.dt);
 
         const iconcode = data.weather[0].icon;
         const iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
@@ -57,10 +76,14 @@ const getData = async () => {
         elWeatherIcon.setAttribute("src", iconUrl);
 
         const elWeatherInfo = document.createElement("p");
-        elWeatherInfo.innerHTML = data.weather[0].description;
+        elWeatherInfo.innerHTML = data.weather[0].description + "<br/>" 
+            + data.main.temp + "degrees" + "<br/>" 
+            + "feels like: " + data.main.feels_like;
 
         const elDiv = document.querySelector("#weatherInfo");
         elDiv.append(elH1WeatherInfo, elWeatherIcon, elWeatherInfo);
+
+        getWeatherRelatedImage(selectedCity, data.weather[0].main);
 
     } catch (err) {
         console.error(err);
