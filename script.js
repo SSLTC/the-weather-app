67ce2c9c256fcde087adb50265c28b3d4343362a
Sven Snoeck <snoeck.sven@live.be>
2023-01-25 07:06:12 +0100

    Basic styling

--- script.js
+++ script.js
@@ -13,12 +13,33 @@ cities.forEach(city => {
     elCity.appendChild(elOption);
 });
 */
+/*
+const date = new Date()
+const currentDate = date.toLocaleDateString("en-u-ca-chinese", {
+    weekday: "short",
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+    timeZone: "CST"
+})
+
+console.log(Intl.DateTimeFormat("en-u-ca-chinese").format(date));
+const currentTime = date.toLocaleTimeString("en-u-ca-chinese", {timeZone: "CST"});
+console.log(currentDate + ' ' + currentTime);
+*/
 
 elCity.addEventListener("change", () => {
     const selectedCity = elCity.options[elCity.selectedIndex].text;
-
-    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=${apiKey}`;
+    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    //const queryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=${apiKey}`;
+    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}`;
     fetch(queryUrl)
     .then((response) => response.json())
     .then((data) => console.log(data));
 })
+
+let timestamp = 1674572400;
+let date = new Date(Date.UTC(1970, 0, 1, 0, 0, timestamp));
+let hour = date.getUTCHours();
+let minute = date.getUTCMinutes();
+console.log(`Hour: ${hour}, Minute: ${minute}`);
