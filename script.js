6b7910c54cab193c5b4d57cdfd91a072c6e7dae0
Snoeck Sven <snoeck.sven@live.be>
2023-01-27 14:02:27 +0100

    ...work in progress...

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//import Chart from 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.0/chart.min.js/auto';
-
 //import cities from './city.list.json' assert {type: 'json'};
 /*
 cities.forEach(city => {
@@ -46,21 +44,34 @@ function setCookie(cookieName, cookieValue, expireDays) {
   }
 
 const apiKey = "1ff8b4b537e2648831257bbfbba69d93";
-const elCity = document.querySelector("#city");
+
+let unit;
 
 checkCookie();
 
-elCity.addEventListener("change", () => {
+const elCity = document.querySelector("#city");
+elCity.addEventListener("change", (event) => {
+  event.preventDefault();
 
-    setCookie("city", elCity.selectedValue, 365);
-    
-    getData();
-    //showWeatherInfo();
+  //const cityName = document.querySelector('.city-input').value;
+  const cityName = elCity.options[elCity.selectedIndex].text;
+
+  setCookie("city", cityName, 365);
+
+  getWeatherRelatedImage(cityName);
+
+  let geoData = fetchGeoData(cityName);
+
+  let weatherData = fetchWeatherData(geoData);
+
+  let hourlyForecastData = fetchHourlyForecastData(geoData);
+
+  showWeatherInfo(weatherData, hourlyForecastData);
     
 })
 
-const getWeatherRelatedImage = async (city, weather) => {
-    const queryUrl = `https://api.unsplash.com/search/photos?client_id=kffooHDtQGZv7a36Az_EaHeWT4CW4D8W3ZINMVOn0hM&query='${city}+${weather}'`;
+const getWeatherRelatedImage = async (cityName) => {
+    const queryUrl = `https://api.unsplash.com/search/photos?client_id=kffooHDtQGZv7a36Az_EaHeWT4CW4D8W3ZINMVOn0hM&query='${cityName}'`;
 
     try {
         const result = await fetch(queryUrl);
@@ -79,63 +90,105 @@ const getWeatherRelatedImage = async (city, weather) => {
     }
 }
 
-getWeatherRelatedImage("belgium", "map");
+getWeatherRelatedImage();
 
-const getData = async () => {
-    const selectedCity = elCity.options[elCity.selectedIndex].text;
-    const unit = "metric";
-    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    //const queryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=${apiKey}`;
-    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=${unit}&appid=${apiKey}`;
+let fetchGeoData = async (cityName) => {
+  try {
+    let result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&units=${unit}&appid=${apiKey}`);
+    let geoData = await result.json();
 
-    try {
-        const result = await fetch(queryUrl);
-        const data = await result.json();
-        console.log(data);
+    unit = "metric";
 
-        const elH1WeatherInfo = document.createElement("h1");
-        elH1WeatherInfo.innerHTML = data.name 
-            + " (" + data.sys.country + ") " + data.weather[0].main + "<br/>"
-            + "min: " + data.main.temp_min + "° max: " + data.main.temp_max + "°"  
-            + "<br/>" + getDateTime(data.dt);
+    console.log(geoData);
 
-        const iconcode = data.weather[0].icon;
-        const iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    return geoData;
 
-        const elWeatherIcon = document.createElement("img");
-        elWeatherIcon.setAttribute("src", iconUrl);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-        const elWeatherInfo = document.createElement("p");
-        elWeatherInfo.innerHTML = data.weather[0].description + "<br/>" 
-            + data.main.temp + "° feels like: " + data.main.feels_like + "°";
+const fetchWeatherData = async (geoData) => {
+  try {
+    const queryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=${unit}&appid=${apiKey}`;
 
-        const elDiv = document.querySelector("#weatherInfo");
-        elDiv.innerHTML = "";
-        elDiv.append(elH1WeatherInfo, elWeatherIcon, elWeatherInfo);
-/*
-        var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-        var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+    let result = await fetch(queryUrl);
+    let weatherData = await result.json();
 
-        const elChart = document.getElementById("myChart");
-        new Chart(elChart, {
-            type: "line",
-            data: {
-                labels: xValues,
-                datasets: [{
-                  backgroundColor: "rgba(0,0,0,1.0)",
-                  borderColor: "rgba(0,0,0,0.1)",
-                  data: yValues
-                }]
-            },
-            options: {}
-          });
-*/
-        getWeatherRelatedImage(selectedCity, data.weather[0].main);
+    console.log(weatherData);
+
+    return weatherData;
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const fetchHourlyForecastData = async (geoData) => {
+
+  const queryUrl = `https://api.openweathermap.org/data/2.5/hourly?lat=${geoData[0].lat}&lon=${geoData[0].lon}&units=${unit}&appid=${apiKey}`;
+
+  try {
+      const result = await fetch(queryUrl);
+      const hourlyForecastData = await result.json();
+
+      console.log(hourlyForecastData);
 
     } catch (err) {
-        console.error(err);
+      console.error(err);
+  }
+}
+
+const showWeatherInfo = (weatherData, hourlyForecastData) => {
+  const elH1WeatherInfo = document.createElement("h1");
+  elH1WeatherInfo.innerHTML = weatherData.name 
+      + " (" + weatherData.sys.country + ") " + weatherData.weather[0].main + "<br/>"
+      + "min: " + weatherData.main.temp_min + "° max: " + weatherData.main.temp_max + "°"  
+      + "<br/>" + getDateTime(weatherData.dt);
+
+  const iconcode = weatherData.weather[0].icon;
+  const iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+  const elWeatherIcon = document.createElement("img");
+  elWeatherIcon.setAttribute("src", iconUrl);
+
+  const elWeatherInfo = document.createElement("p");
+  elWeatherInfo.innerHTML = weatherData.weather[0].description + "<br/>" 
+      + weatherData.main.temp + "° feels like: " + weatherData.main.feels_like + "°";
+
+  const elCanvas = document.createElement("canvas");
+  elCanvas.setAttribute("id", "myChart");
+
+  const elDiv = document.querySelector("#weatherInfo");
+  elDiv.innerHTML = "";
+  elDiv.append(elH1WeatherInfo, elWeatherIcon, elWeatherInfo, elCanvas);
+  
+  const elChart = document.querySelector("#myChart");
+
+  var xValues = [100,200,300,400,500,600,700,800,900,1000];
+
+  let myChart = new Chart(elChart, {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{ 
+        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+        borderColor: "red",
+        fill: false
+      }, { 
+        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+        borderColor: "green",
+        fill: false
+      }, { 
+        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+        borderColor: "blue",
+        fill: false
+      }]
+    },
+    options: {
+      legend: {display: false}
     }
-    
+  });
 }
 
 /*
